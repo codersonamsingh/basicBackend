847a5c1d3cebae6b994b9ec7108fe114554aa11c
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-07-31 20:49:14 +0530

    call routes completed

---
+++ routes/api/auth/login.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = express.Router();
+
+
+router.get("/sendOtp", (req,res)=> {
+    res.json({
+        message:"otp send",
+        varient:"success"
+
+    })
+
+
+
+})
