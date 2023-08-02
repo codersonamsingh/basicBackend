52dc9ec8ec1f6f9e22f45bd4df7e828bc95902aa
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-02 20:53:01 +0530

    jsonwtstrategy run

--- strategies/jsonwtstrategy.js
+++ strategies/jsonwtstrategy.js
@@ -11,21 +11,19 @@ opts.secretOrKey = myKey.secret;
 
 module.exports = passport => {
     passport.Use(
-        new JwtStrategy(opts, (jwt_payload, done)=>{
+        new JwtStrategy(opts, (jwt_payload, done) =>{
             User.findById(jwt_payload.id)
-            .then(user =>{
-                if (user){
-                    return done(null,user)
+            .then(user => {
+                if (user) {
+                    return done(null, user)
                 }
-                return done(null,false)
+                return done(null, false)
             })
-            .catch(err => console.log(err))
-
+            .catch(err => console.log(err));
         }
         ) 
-        
-        
-        )}
+        );
+    }
 
 
 
