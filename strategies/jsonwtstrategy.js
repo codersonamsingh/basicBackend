3e645d94ace58ca49d6eb04fc11a44d1336c1556
codersonamsingh <sonam.sin.rajput@gmail.com>
2023-08-02 21:11:58 +0530

    json

--- strategies/jsonwtstrategy.js
+++ strategies/jsonwtstrategy.js
@@ -10,20 +10,19 @@ opts.jsonfromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = myKey.secret;
 
 module.exports = passport => {
-    passport.Use(
+    passport.use(
         new JwtStrategy(opts, (jwt_payload, done) =>{
             User.findById(jwt_payload.id)
             .then(user => {
                 if (user) {
-                    return done(null, user)
+                    return done(null, user);
                 }
-                return done(null, false)
+                return done(null, false);
             })
             .catch(err => console.log(err));
-        }
-        ) 
-        );
-    }
+    }) 
+  );
+};
 
 
 
